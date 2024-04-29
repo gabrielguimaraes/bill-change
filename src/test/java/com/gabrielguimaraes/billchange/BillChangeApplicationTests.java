75559545e9a3fe706748e5bed8ac7bf148af24b6
Gabriel Guimaraes <gabguim@gmail.com>
2024-04-29 10:19:40 +0200

    chore: adding spring boot 3

---
+++ src/test/java/com/gabrielguimaraes/billchange/BillChangeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabrielguimaraes.billchange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillChangeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
